857fc7aefef86dbeffcbbfcc4a6fbddc489bdbc4
Maxkaizo <jlmartinezol@outlook.com>
2025-06-18 00:48:50 -0600

    new file:   04_deployment/lesson/streaming/Dockerfile 	new file:   04_deployment/lesson/streaming/Pipfile 	new file:   04_deployment/lesson/streaming/Pipfile.lock 	new file:   04_deployment/lesson/streaming/__pycache__/lambda_function.cpython-312.pyc 	modified:   04_deployment/lesson/streaming/commands.txt 	new file:   04_deployment/lesson/streaming/lambda_function.py 	new file:   04_deployment/lesson/streaming/mlruns/0/meta.yaml 	new file:   04_deployment/lesson/streaming/test.py 	new file:   04_deployment/lesson/streaming/test_docker.py

---
+++ 04_deployment/lesson/streaming/test.py
@@ -0,0 +1,25 @@
+import lambda_function
+
+event = {
+    "Records": [
+        {
+            "kinesis": {
+                "kinesisSchemaVersion": "1.0",
+                "partitionKey": "1",
+                "sequenceNumber": "49664349626995788546610880530340811045745011354364805122",
+                "data": "ewogICAgInJpZGUiOiB7CiAgICAgICAgIlBVTG9jYXRpb25JRCI6IDEzMCwKICAgICAgICAiRE9Mb2NhdGlvbklEIjogMjA1LAogICAgICAgICJ0cmlwX2Rpc3RhbmNlIjogMy42NgogICAgfSwgCiAgICAicmlkZV9pZCI6IDE1Ngp9",
+                "approximateArrivalTimestamp": 1750203556.452
+            },
+            "eventSource": "aws:kinesis",
+            "eventVersion": "1.0",
+            "eventID": "shardId-000000000000:49664349626995788546610880530340811045745011354364805122",
+            "eventName": "aws:kinesis:record",
+            "invokeIdentityArn": "arn:aws:iam::438480637738:role/lambda-kinesis-role",
+            "awsRegion": "us-west-1",
+            "eventSourceARN": "arn:aws:kinesis:us-west-1:438480637738:stream/ride_events_calif"
+        }
+    ]
+}
+
+result = lambda_function.lambda_handler(event, None)
+print(result)
