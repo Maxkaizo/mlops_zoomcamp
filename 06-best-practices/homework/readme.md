b1139eb1401ed3edfe6264e1530538883d02d52d
Maxkaizo <jlmartinezol@outlook.com>
2025-07-02 00:21:31 -0600

    new file:   06-best-practices/homework/.gitignore 	new file:   06-best-practices/homework/Dockerfile 	new file:   06-best-practices/homework/Pipfile 	new file:   06-best-practices/homework/Pipfile.lock 	new file:   06-best-practices/homework/batch.py 	new file:   06-best-practices/homework/image.png 	new file:   06-best-practices/homework/model.bin 	new file:   06-best-practices/homework/readme.md 	new file:   06-best-practices/homework/temp.ipynb 	new file:   06-best-practices/homework/tests/__init__.py 	new file:   06-best-practices/homework/tests/test_batch.py

---
+++ 06-best-practices/homework/readme.md
@@ -0,0 +1,31 @@
+# Homework 
+
+## Q1. Refactoring
+
+* How does the if statement that we use for this looks like?
+
+### Q1. Solution
+
+* if __name__ == "__main__":
+
+![alt text](image.png)
+
+
+## Q2. Installing pytest
+
+Next, create a folder tests and create two files. One will be the file with tests. We can name it test_batch.py.
+
+* What should be the other file?
+
+### Q2. Solution
+
+* __init__.py
+
+## Q3. Writing first unit test
+
+How many rows should be there in the expected dataframe?
+
+* 1
+* 2
+* 3
+* 4
---
+++ 06-best-practices/homework/tests/test_batch.py
@@ -0,0 +1,37 @@
+import batch
+import pandas as pd
+
+from datetime import datetime
+
+def dt(hour, minute, second=0):
+    return datetime(2023, 1, 1, hour, minute, second)
+
+def test_prepare_data():
+    data = [
+    (None, None, dt(1, 1), dt(1, 10)),
+    (1, 1, dt(1, 2), dt(1, 10)),
+    (1, None, dt(1, 2, 0), dt(1, 2, 59)),
+    (3, 4, dt(1, 2, 0), dt(2, 2, 1)),      
+    ]
+
+    columns = ['PULocationID', 'DOLocationID', 'tpep_pickup_datetime', 'tpep_dropoff_datetime']
+    categorical = ['PULocationID', 'DOLocationID']
+
+    df = pd.DataFrame(data, columns=columns)
+
+    expected_dict = {
+        'PULocationID': {0: '-1', 1: '1'},
+        'DOLocationID': {0: '-1', 1: '1'},
+        'tpep_pickup_datetime': {0: Timestamp('2023-01-01 01:01:00'), 1: Timestamp('2023-01-01 01:02:00')},
+        'tpep_dropoff_datetime': {0: Timestamp('2023-01-01 01:10:00'), 1: Timestamp('2023-01-01 01:10:00')},
+        'duration': {0: 9.0, 1: 8.0}
+        }
+
+    expected_result = pd.DataFrame(expected_dict)
+
+    actual_result = batch.prepare_data(df, categorical)
+
+    assert expected_result == actual_result
+
+
+
